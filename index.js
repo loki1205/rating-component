4a1956ba906bcc7e1b8e88117aaa80817a383c72
loki1205 <kloknath389@gmail.com>
2023-02-18 23:31:56 +0530

    Version 1.0

---
+++ index.js
@@ -0,0 +1,30 @@
+const numbers = document.querySelectorAll(".number");
+var count;
+for (let number of numbers)
+{
+    number.onclick = function(){
+        for (let number2 of numbers)
+        {
+            number2.classList.remove("selected");
+            number2.classList.remove("no-hover");
+            number.id = "selected"
+        }
+        number.classList.add("selected");
+        number.classList.add("no-hover");
+        count = number.textContent;
+        localStorage.setItem("count",count);
+    }
+}
+
+document.getElementById("submit").onclick = function(){
+    var f = document.getElementById("frm");
+    if(count===0 || count==undefined)
+    {
+        alert("Please rate us!")
+    }
+    else
+    {   
+        f.setAttribute('action',"/completed.html");
+    }
+    return true;
+}
---
+++ style-guide.md
@@ -0,0 +1,38 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+
+
+- Font size (paragraph): 15px
+
+### Font
+
+- Family: [Overpass](https://fonts.google.com/specimen/Overpass)
+- Weights: 400, 700
+
+
+  <!-- How did we do?
+
+  Please let us know how we did with your support request. All feedback is appreciated 
+  to help us improve our offering!
+
+  1 2 3 4 5
+
+  Submit
+
+  You selected out of 5
+
+  Thank you!
+
+  We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch! -->
